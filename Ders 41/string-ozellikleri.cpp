7d91ac9ae69025c09b29a9c7f7cb7027d3882449
Raldexx <muhammedulgu2234@gmail.com>
2024-11-15 17:09:18 +0300

    e.
    15.11.24

---
+++ Ders 41/string-ozellikleri.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int z; //hata vermesin diye gereksiz birşey boşver
+    
+    //Normalde + sayıları toplarken stringde :
+
+    /*string x,y,z;
+    x="20";         string ekleyince sayıları "" içine al
+    y="70";                  
+    z=x+y;     stringde plus operatörü sayıları birleştirir */
+
+
+
+     //Peki int ile string toplanırsa ne olur?
+     /*string x,z;
+    x="20";         
+    int y=70;    //Hata Alırız :D
+    z=x+y;*/
+
+
+    cout<<z<<"\n";
+    return 0;
+    }
---
+++ Ders 41/string_uzunlugunu_bulma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main(){
+    string txt="Neredeydin?";
+    cout<<"Uzunluk: "<<txt.size()<<"\n"; //size içindeki harfleri veya sayıları sayar boşluklarda karakterdir!
+ 
+return 0;
+}
